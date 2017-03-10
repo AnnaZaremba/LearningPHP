3cedb1c4232dcb687692109fed7724423c1c1942
Anna Zaremba <aniazarem@gmail.com>
2017-03-10 20:21:42 +0100

    wysyłanie maila - nowa strona

--- montujemymeble/mail.php
+++ montujemymeble/mail.php
@@ -12,67 +12,69 @@
 <body>
 <div id="home">
     <div class="headerLine">
+        <div class="logo col-md-4" style="margin-left: 385px">
+            <div >
+                <a href="montujemyMebleIndex.php" >
+                    <img src="images/logoMM.png">
+                </a>
+            </div>
+            <div class="col-md-4 Des">
+                <h3>
+                    <?php
+                    include_once 'KontaktWalidator.php';
+                    include_once 'KontaktWysylka.php';
+
+                    $kontaktWalidator = new KontaktWalidator();
+                    $kontaktWysylka = new KontaktWysylka();
+
+                    $formularzWypelniony = false;
+
+                    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+                        $formularzWypelniony = true;
+                    }
+
+                    if ($formularzWypelniony) {
+                        $imie = $_POST['name'];
+                        $email = $_POST['email'];
+                        $temat = $_POST['subject'];
+                        $tresc = $_POST['message'];
+
+                        $komunikatyBledu = [];
+                        if (!$kontaktWalidator->sprawdzEmail($email)) {
+                            $komunikatyBledu[] = "Niepoprawny adres e-mail";
+                        }
+
+                        if (!$kontaktWalidator->sprawdzImie($imie)) {
+                            $komunikatyBledu[] = "Niepoprawne imię";
+                        }
+                        if (!$kontaktWalidator->sprawdzTemat($temat)) {
+                            $komunikatyBledu[] = "Niepoprawny temat";
+                        }
+
+                        if (!$kontaktWalidator->sprawdzTresc($tresc)) {
+                            $komunikatyBledu[] = "Niepoprawna treść";
+                        }
+                        if (count($komunikatyBledu) > 0) {
+
+                            foreach ($komunikatyBledu as $value) {
+                                echo "<div style='font-size: large;'>" . $value . "</div>";
+                            }
+                        } else {
+                            if ($kontaktWysylka->wyslijMaila($temat, $tresc, $imie, $email)) {
+                                echo "<div style='font-size: large;'>Wiadomość została wysłana - dziekujemy!</div>";
+                            } else {
+                                echo "Błąd wysyłania";
+                            }
+                        }
+                    }
+                    ?>
+                </h3>
+            </div>
         <div id="menuF" class="default">
             <div class="container">
                 <div class="row Ama">
-                    <div class="logo col-md-4" style="margin-left: 385px">
-                        <div >
-                            <a href="montujemyMebleIndex.php" >
-                                <img src="images/logoMM.png">
-                            </a>
-                        </div>
-                        <div class="col-md-4 Des">
-                            <h3>
-                                <?php
-                                include_once 'KontaktWalidator.php';
-                                include_once 'KontaktWysylka.php';
 
-                                $kontaktWalidator = new KontaktWalidator();
-                                $kontaktWysylka = new KontaktWysylka();
 
-                                $formularzWypelniony = false;
-
-                                if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
-                                    $formularzWypelniony = true;
-                                }
-
-                                if ($formularzWypelniony) {
-                                    $imie = $_POST['name'];
-                                    $email = $_POST['email'];
-                                    $temat = $_POST['subject'];
-                                    $tresc = $_POST['message'];
-
-                                    $komunikatyBledu = [];
-                                    if (!$kontaktWalidator->sprawdzEmail($email)) {
-                                        $komunikatyBledu[] = "Niepoprawny adres e-mail";
-                                    }
-
-                                    if (!$kontaktWalidator->sprawdzImie($imie)) {
-                                        $komunikatyBledu[] = "Niepoprawne imię";
-                                    }
-                                    if (!$kontaktWalidator->sprawdzTemat($temat)) {
-                                        $komunikatyBledu[] = "Niepoprawny temat";
-                                    }
-
-                                    if (!$kontaktWalidator->sprawdzTresc($tresc)) {
-                                        $komunikatyBledu[] = "Niepoprawna treść";
-                                    }
-                                    if (count($komunikatyBledu) > 0) {
-
-                                        foreach ($komunikatyBledu as $value) {
-                                            echo "<div style='font-size: large;'>" . $value . "</div>";
-                                        }
-                                    } else {
-                                        if ($kontaktWysylka->wyslijMaila($temat, $tresc, $imie, $email)) {
-                                            echo "<div style='font-size: large;'>Wiadomość została wysłana - dziekujemy!</div>";
-                                        } else {
-                                            echo "Błąd wysyłania";
-                                        }
-                                    }
-                                }
-                                ?>
-                            </h3>
-                        </div>
                     </div>
                 </div>
             </div>
